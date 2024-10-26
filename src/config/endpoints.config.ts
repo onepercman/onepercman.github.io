0934ab104ef976a77eada409330f243b2c904425
onepercman ‎ <onepercman@192.168.1.32.non-exists.ptr.local>
2024-10-26 15:37:55 +0700

    🚀 update repo

--- src/config/endpoints.config.ts
+++ src/config/endpoints.config.ts
@@ -1,3 +1,2 @@
 export const API_URL = import.meta.env.VITE_API_URL
-export const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN
-console.log("🚀 ~ GITHUB_TOKEN:", GITHUB_TOKEN)
+export const GITHUB_TOKEN = import.meta.env.VITE_GH_TOKEN
